854cf5f9404734b2d604266c8786eb426a8e69a8
Suresh Raj <sureshraj@posoco.srldc.org>
2020-03-18 10:26:31 +0530

    Added inserting into Database

--- weather.py
+++ weather.py
@@ -1,6 +1,8 @@
 from selenium import webdriver 
 import time 
 from selenium.webdriver.chrome.options import Options
+import pyodbc
+from datetime import datetime
 
 
 WINDOW_SIZE = "1920,1080"
@@ -11,8 +13,9 @@ chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
   
 # set webdriver path here it may vary 
 # ,options=chrome_options
-brower = webdriver.Chrome(executable_path ='C:/Users/dilee/Downloads/chromedriver_win32/chromedriver.exe') 
-  
+# brower = webdriver.Chrome(executable_path ='C:/Users/dilee/Downloads/chromedriver_win32/chromedriver.exe') 
+brower = webdriver.Chrome(executable_path ='F:/DILEEP/Weather/SeleniumWebDriverPython/chromedriver.exe') 
+ 
 website_URL ="https://www.meteoblue.com/en/weather/week/jakarta_indonesia_1642911"
 brower.get(website_URL) 
 print(brower.title)
@@ -29,6 +32,8 @@ time.sleep(10)
 # for a particular time 
 # you have to figure that out 
 # brower.close()
+db_con='DRIVER={SQL Server Native Client 11.0};SERVER=SURESHRAJ;DATABASE=Weather;UID=sa;PWD=user@123; MARS_Connection=yes;'
+Weather_Data_Con=pyodbc.connect(db_con)
 
 
 table_id=brower.find_element_by_class_name("hourlywind")
@@ -43,3 +48,12 @@ for row in rows:
 print(Data)
     # col = row.find_elements_by_tag_name("td")[1] #note: index start from 0, 1 is col 2
     # print(col.text)#prints text from the element
+
+for i in range(0,len(Data[0])):
+	current_date=datetime.now().strftime("%Y-%m-%d")
+	sql="INSERT into Weather_Data (Date, Hour, Temp, Wind_Speed, Wind_Gust) VALUES \
+	('"+current_date+"', '"+Data[0][i]+"', '"+Data[3][i]+"', '"+Data[5][i]+"', '"+Data[6][i]+"')"
+	Weather_Data_Con.execute(sql)
+
+Weather_Data_Con.commit()
+Weather_Data_Con.close()
